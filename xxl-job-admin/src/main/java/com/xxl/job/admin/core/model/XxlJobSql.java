d43313fc70ef88dbc80dbd6a5b2ef9c659ddc91d
jiadongpo <295445156@qq.com>
2017-06-28 15:29:03 +0800

    XxlJob

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/model/XxlJobSql.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/model/XxlJobSql.java
@@ -5,50 +5,49 @@ import java.util.List;
 
 public class XxlJobSQL {
 
-    private int id;
-    private String taskName;
-    private String dataSource;
-    private String sqlList;
-    private List<XxlJobSubSQL> xxlJobSubSQLs = new ArrayList<XxlJobSubSQL>();
+	private int id;
+	private String taskName;
+	private String dataSource;
+	private String sqlList;
+	private List<XxlJobSubSQL> xxlJobSubSQLs = new ArrayList<XxlJobSubSQL>();
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public String getTaskName() {
-        return taskName;
-    }
+	public String getTaskName() {
+		return taskName;
+	}
 
-    public String getDataSource() {
-        return dataSource;
-    }
+	public String getDataSource() {
+		return dataSource;
+	}
 
-    public void setSqlList(String sqlList) {
-        this.sqlList = sqlList;
-    }
+	public String getSqlList() {
+		return sqlList;
+	}
 
-    public String getSqlList() {
+	public void setSqlList(String sqlList) {
+		this.sqlList = sqlList;
+	}
 
-        return sqlList;
-    }
+	public List<XxlJobSubSQL> getXxlJobSubSQLs() {
+		return xxlJobSubSQLs;
+	}
 
-    public List<XxlJobSubSQL> getXxlJobSubSQLs() {
-        return xxlJobSubSQLs;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
 
-    public void setTaskName(String taskName) {
-        this.taskName = taskName;
-    }
+	public void setDataSource(String dataSource) {
+		this.dataSource = dataSource;
+	}
 
-    public void setDataSource(String dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    public void setXxlJobSubSQLs(List<XxlJobSubSQL> xxlJobSubSQLs) {
-        this.xxlJobSubSQLs = xxlJobSubSQLs;
-    }
+	public void setXxlJobSubSQLs(List<XxlJobSubSQL> xxlJobSubSQLs) {
+		this.xxlJobSubSQLs = xxlJobSubSQLs;
+	}
 }
